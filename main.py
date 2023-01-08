599467bae1cb4d00a0704c28b5724a0716423682
utibori-jp <kouki.utibori@gmail.com>
2023-01-08 12:47:54 +0900

    [delete]

--- main.py
+++
@@ -1,9 +0,0 @@
-from dataset import DataSet
-from sidebar import sidebar
-import field
-import streamlit as st
-
-st.title("Machine Learning App with Titanic")
-dataset = DataSet()
-sidebar_info = sidebar(dataset)
-field.main_field(dataset, sidebar_info)
