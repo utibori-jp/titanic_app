c54224850a68450e45bed29822c85dd8349b701d
utibori-jp <kouki.utibori@gmail.com>
2023-01-08 11:22:20 +0900

    [Change]dataset.py

--- dataset.py
+++ dataset.py
@@ -3,7 +3,7 @@ from data_processer import preprocessing
 
 class DataSet:
     def __init__(self):
-        root_path = "/Users/koki/Documents/streamlit/titanic_app/titanic/"
+        root_path = "rootpath/titanic/"
         self.train_data = pd.read_csv(root_path + "train.csv")
         self.test_data = pd.read_csv(root_path + "test.csv")
         self.y_test = pd.read_csv(root_path + "gender_submission.csv")
